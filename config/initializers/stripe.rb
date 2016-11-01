84da96d812d926e2cce4a53fc6747ad09f3626ef
Karen Rooney <karaignee@gmail.com>
2016-11-01 11:07:07 +0000

    restart

--- config/initializers/stripe.rb
+++
@@ -1,6 +0,0 @@
-# configuration.stripe = {
-#   :publishable_key => ENV['PUBLISHABLE_KEY'],
-#   :secret_key      => ENV['SECRET_KEY']
-# }
-
-# Stripe.api_key = Rails.configuration.stripe[:secret_key]
